a89bc66d9c6a9973b9fb3e15d95dc87a29e8eaa2
oscar-shing <oscar199617@gmail.com>
2024-08-10 01:33:12 +0800

    Adjusting the Ship’s Speed in 'settings.py' and modifying 'ship.py'

--- ship.py
+++ ship.py
@@ -6,6 +6,7 @@ class Ship:
     def __init__(self, ai_game):
         """Initialize the ship and set its starting position."""
         self.screen = ai_game.screen
+        self.settings = ai_game.settings
         self.screen_rect = ai_game.screen.get_rect()
 
         # Load the ship image and get its rect.
@@ -15,6 +16,9 @@ class Ship:
         # Start each new ship at the bottom center of the screen.
         self.rect.midbottom = self.screen_rect.midbottom
 
+        # Store a float for the ship's exacthorizontal position.
+        self.x = float(self.rect.x)
+
         # Movement flags; start with a ship that's not moving.
         self.moving_right = False
         self.moving_left = False
@@ -22,10 +26,15 @@ class Ship:
 
     def update(self):
         """Update the ship's position based on the movement flags."""
+        # Update the ship's x value, not the rect.
         if self.moving_right:
+            self.x += self.settings.ship_speed
             self.rect.x += 1
         if self.moving_left:
-            self.rect.x -= 1
+            self.x -= self.settings.ship_speed
+
+        # Update rect object from self.x.
+        self.rect.x = self.x
 
     def blitme(self):
         """Draw the ship at its current location."""
