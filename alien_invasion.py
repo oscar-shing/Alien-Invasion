6538eb871fe900815ccf85cab9a81621d22afff5
oscar-shing <oscar199617@gmail.com>
2024-08-13 18:54:01 +0800

    Deleting extra spcaes in 'alien_invasion.py'

--- alien_invasion.py
+++ alien_invasion.py
@@ -18,7 +18,7 @@ class AlienInvasion:
         self.screen = pygame.display.set_mode(
             (self.settings.screen_width, self.settings.screen_height))
         
-                """Running the game in fullscreen mode"""
+        """Running the game in fullscreen mode"""
         # self.screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
         # self.settings.screen_width = self.screen.get_rect().width
         # self.settings.screen_height = self.screen.get_rect().height
