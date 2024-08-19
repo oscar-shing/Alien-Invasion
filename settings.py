3adbcb3a8bcc754fc904172f56edea022cffc3c4
oscar-shing <oscar199617@gmail.com>
2024-08-19 13:34:41 +0800

    Scoring: Increasing Point Values by modifying 'settings.py'

--- settings.py
+++ settings.py
@@ -13,17 +13,19 @@ class Settings:
         self.ship_limit = 3
 
         # Bullet settings
-        self.bullet_speed = 3.5
+        self.bullet_speed = 30.5
         self.bullet_width = 3
         self.bullet_height = 15
         self.bullet_color = (60, 60, 60)
-        self.bullets_allowed = 10
+        self.bullets_allowed = 30
 
         # Alien settings
         self.fleet_drop_speed = 10
 
         # How quickly the game speeds up
         self.speedup_scale = 1.1
+        # How quickly the alien point values increase
+        self.score_scale = 1.5
 
         self.initialize_dynamic_settings()
 
@@ -31,7 +33,7 @@ class Settings:
         """Initialize settings that change throughout the game."""
         self.ship_speed = 1.5
         self.bullet_speed = 2.5
-        self.alien_speed = 30.0
+        self.alien_speed = 3.0
 
         # fleet_direction of 1 represents right; -1 represents left.
         self.fleet_direction = 1
@@ -40,7 +42,9 @@ class Settings:
         self.alien_points = 50
 
     def increase_speed(self):
-        """Increase speed settings."""
+        """Increase speed settings and alien point values."""
         self.ship_speed *= self.speedup_scale
         self.bullet_speed *= self.speedup_scale
         self.alien_speed *= self.speedup_scale
+
+        self.alien_points = int(self.alien_points * self.score_scale)
