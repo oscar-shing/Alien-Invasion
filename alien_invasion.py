b55ae68261e545db2734f2e3029aeb7e2fe9d103
oscar-shing <oscar199617@gmail.com>
2024-08-19 13:49:11 +0800

    Scoring: High Score by modifying 'scoreboard.py', 'alien_invasion.py' and 'game_stats.py'

--- alien_invasion.py
+++ alien_invasion.py
@@ -140,7 +140,8 @@ class AlienInvasion:
         if collisions:
             for aliens in collisions.values():
                 self.stats.score += self.settings.alien_points * len(aliens)
-            self.sb.prep_score()        
+            self.sb.prep_score()
+            self.sb.check_high_score()
 
         if not self.aliens:
             # Destroy existing bullets and create new fleet.
